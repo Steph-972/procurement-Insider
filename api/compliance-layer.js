1215eafd5e1956d3f0573ae7d231542de541765b
Steph-972 <loeildelacheteur@gmail.com>
2026-05-17 22:28:55 -0400

    Add minimal legal links layer

--- api/compliance-layer.js
+++ api/compliance-layer.js
@@ -1,5 +1,10 @@
 function applyComplianceLayer(html) {
-  return String(html || '');
+  var out = String(html || '');
+  if (out.indexOf('pi-legal-links') === -1) {
+    var links = '<div id="pi-legal-links"><a href="/mentions-legales">Mentions legales</a> <a href="/conditions-generales-prestations-services">CGPS</a> <a href="/politique-confidentialite">Confidentialite</a> <a href="/politique-cookies">Cookies</a> <a href="/deontologie">Deontologie</a></div>';
+    out = out.replace('</body>', links + '</body>');
+  }
+  return out;
 }
 
 module.exports = { applyComplianceLayer };
